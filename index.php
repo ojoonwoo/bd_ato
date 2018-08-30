0e54cb4f55bc709f904bb539e5ea28458c912ba7
kyhfan <kyhfan@gmail.com>
2018-08-30 16:42:26 +0900

    111

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     include_once "./include/autoload.php";
 
     $mnv_f 			= new mnv_function();
-//    $my_db         = $mnv_f->Connect_MySQL();
+   	$my_db         = $mnv_f->Connect_MySQL();
     $mobileYN      = $mnv_f->MobileCheck();
     // $obYN          = $mnv_f->BrowserCheck();
     $IEYN          = $mnv_f->IECheck();
