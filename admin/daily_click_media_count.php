713e8e25154403683e6c575345f7fa5a475d2859
kyhfan <kyhfan@gmail.com>
2018-09-20 18:10:27 +0900

    2222

--- admin/daily_click_media_count.php
+++ admin/daily_click_media_count.php
@@ -21,7 +21,7 @@
     <!-- Page Heading -->
       <div class="row">
         <div class="col-lg-12">
-          <h1 class="page-header">캠페인 이벤트 참여 버튼 클릭 수 PC / Mobile</h1>
+          <h1 class="page-header">매체별 이벤트 참여 버튼 클릭 수 PC / Mobile</h1>
         </div>
       </div>
       <!-- /.row -->
